270e385104afdb15c7344592a6c7bc2dc44f6290
Hammad-hab <hammadalibutt30@gmail.com>
2025-12-16 16:49:45 +0500

    init gtk binding gen mechanism, buggy for now

---
+++ bindgen.py
@@ -0,0 +1,147 @@
+import json
+
+
+def define_type(ptype: str):
+    param_type = ptype
+    if ptype.startswith('int') and not ptype.endswith('*'):
+            param_type = param_type.replace('int', 'Int')
+    elif ptype.startswith('uint') and not ptype.endswith('*'):
+            param_type = param_type.replace('uint', 'UInt')
+    elif 'float' in ptype or 'double' in ptype:
+            param_type = 'Float32'
+    elif 'char*' in ptype or 'char*[]' in ptype:
+            param_type = "String"
+    elif 'void' in ptype:
+            param_type = 'None'
+    elif 'boolean' in ptype:
+            param_type = 'Bool'
+    elif ptype.endswith('*'):
+            param_type = f"LegacyUnsafePointer[{define_type(param_type.replace('*', ''))}]"
+    elif 'Widget' in ptype:
+            param_type = 'GTKInterface'
+    return param_type
+
+
+def generate_fn_params(params: dict[str, str]):
+    param_names = params.keys()
+    param_str = ""
+
+    for pname in param_names:
+        param_type = define_type(params[pname])
+        param_str += f'{pname}: {param_type}, '
+    
+    return param_str
+
+
+def extract_param_names(params: dict[str, str]):
+    parameters = list(params.keys())
+    return parameters
+
+def fix_parameters(params: dict[str, str]) -> dict[str, str]:
+    parameters = list(params.keys())
+    nw_parameters = {}
+    for p in parameters:
+        if ('def' in p):
+            new_name = f'x_{p}'
+            nw_parameters[new_name] = params[p]
+        else:
+            nw_parameters[p] = params[p]
+
+    return nw_parameters
+
+def extract_param_values(params: dict[str, str]):
+    return list(params.values())
+
+def generate_function(name: str, return_type: str, params: dict[str, str]): 
+    param_names = extract_param_names(params)
+    # param_types = extract_param_values(params)
+    
+    string_types_parameters = list(filter(lambda name: 'char*' in params[name] or 'char*[]' in params[name], param_names))
+    string_preprocess_ijection = ""
+
+    if len(string_types_parameters) > 0:
+        for parameter_name in string_types_parameters:
+            new_parameter_name = f'cstr_{parameter_name}'
+            position = (param_names).index(parameter_name)
+            string_preprocess_ijection += f'\n\tvar slc_{new_parameter_name} = StringSlice({parameter_name} + "\0")'
+            string_preprocess_ijection += f'\n\tvar {new_parameter_name} = CStringSlice(slc_{new_parameter_name})'
+            param_names[position] = new_parameter_name
+
+    return_type_mojo = define_type(return_type)
+    if return_type_mojo == 'None':
+        return_type_mojo = 'NoneType'
+    elif return_type_mojo == 'String':
+        return_type_mojo = 'LegacyUnsafePointer[c_char]'
+        
+    params_passed = ' '.join([f"{name}," for name in param_names])
+    return (f"fn {name}({generate_fn_params(params)}) raises -> {return_type_mojo}:{string_preprocess_ijection}\n\treturn external_call[\"{name}\", {return_type_mojo}]({params_passed})")
+    ...
+
+def declare_legacy_ptr(type: str):
+    return f'LegacyUnsafePointer[{type}]'
+
+def declare_comptime(name: str, value: str):
+    return f'comptime {name}={value}'
+
+def declare_gtk_ptr_comptime(name: str, type: str):
+    return declare_comptime(name, declare_legacy_ptr(type))
+
+def declare_whitelisted_functions():
+    WHITELIST = {
+        'g_application_run': {
+            'rtype': 'int32',
+            'params': {
+                'app': "GTKInterface",
+                'argc': 'int32', 
+                'argv': 'GTKType'
+            }
+        },
+        'g_signal_connect_data': {
+            'rtype': 'uint64',
+            'params': {
+                'instance': 'GTKInterface',
+                'detailed_signal': 'char*[]',
+                'c_handler': 'GTKType',
+                'data': 'GTKType',
+                'destroy_data': 'NoneType',
+                'connect_flags': 'uint32'
+            }
+        }
+    }
+
+    functions_names = WHITELIST.keys()
+    return declare_functions(functions_names, WHITELIST)
+
+def declare_functions(functions_names, functions: dict[str]):
+    mojo_bindings = ''
+    for function_name in functions_names:
+        if function_name == 'unique_types': 
+            continue # unique types is not a function 
+        descriptor: dict = functions[function_name]
+        params: dict[str, str] = fix_parameters(descriptor['params'])
+        
+        rtype: str = descriptor['rtype']
+        fn = generate_function(function_name, rtype, params)
+        mojo_bindings += fn + '\n'
+        # with open('bindings.mojo', 'w') as f:
+        #     f.write(mojo_bindings)
+    return mojo_bindings 
+
+with open('fn.json', 'r') as f:
+    functions: dict = json.loads(f.read())
+    functions_names = functions.keys()
+    types: list[str] = functions['unique_types']
+    comptimes = ''
+    for type in types:
+        if type.endswith('@32'):
+            comptimes += declare_comptime(type.replace('@32', ''), 'Int32') + '\n'
+      
+
+    mojo_bindings = f"from sys.ffi import external_call, c_char, CStringSlice\n{comptimes}\ncomptime GTKInterface = LegacyUnsafePointer[NoneType]\ncomptime GTKType=LegacyUnsafePointer[NoneType]\ncomptime GError=LegacyUnsafePointer[NoneType]\ncomptime filename=String\n"
+
+    mojo_bindings += declare_functions(functions_names, functions)
+    
+    whitelisted_declarations = declare_whitelisted_functions()
+    mojo_bindings += '\n' + whitelisted_declarations
+    with open('bindings.mojo', 'w') as f:
+        f.write(mojo_bindings)
---
+++ descriptgen.py
@@ -0,0 +1,333 @@
+from gi.repository import GIRepository
+import json
+
+types = set()
+
+def get_type_string(type_info):
+    """Convert a TypeInfo to a readable string"""
+    tag = type_info.get_tag()
+  
+    if tag == GIRepository.TypeTag.VOID:
+        return "void"
+    elif tag == GIRepository.TypeTag.BOOLEAN:
+        return "boolean"
+    elif tag == GIRepository.TypeTag.INT8:
+        return "int8"
+    elif tag == GIRepository.TypeTag.UINT8:
+        return "uint8"
+    elif tag == GIRepository.TypeTag.INT16:
+        return "int16"
+    elif tag == GIRepository.TypeTag.UINT16:
+        return "uint16"
+    elif tag == GIRepository.TypeTag.INT32:
+        return "int32"
+    elif tag == GIRepository.TypeTag.UINT32:
+        return "uint32"
+    elif tag == GIRepository.TypeTag.INT64:
+        return "int64"
+    elif tag == GIRepository.TypeTag.UINT64:
+        return "uint64"
+    elif tag == GIRepository.TypeTag.FLOAT:
+        return "float"
+    elif tag == GIRepository.TypeTag.DOUBLE:
+        return "double"
+    elif tag == GIRepository.TypeTag.UTF8:
+        return "char*"
+    elif tag == GIRepository.TypeTag.FILENAME:
+        return "char*"
+    elif tag == GIRepository.TypeTag.ARRAY:
+        array_type = type_info.get_array_type()
+        param_type = type_info.get_param_type(array_type)
+        typestr = f"{get_type_string(param_type)}[]"
+        return typestr
+    elif tag == GIRepository.TypeTag.INTERFACE:
+        iface = type_info.get_interface()
+
+        if isinstance(iface, GIRepository.EnumInfo):
+            types.add(iface.get_name() + '@32')
+            return iface.get_name()        # or UInt32 / Int32
+
+        if isinstance(iface, GIRepository.FlagsInfo):
+            types.add(iface.get_name() + '@I')
+            return iface.get_name()       # bitflags → integer
+
+        return 'GTKInterface'
+    elif tag == GIRepository.TypeTag.GLIST:
+        types.add('GTKType')
+        # param_type = type_info.get_param_type(0)
+        return f"GTKType"
+    elif tag == GIRepository.TypeTag.GTYPE:
+        types.add('GTKType')
+        # param_type = type_info.get_param_type(0)
+        return f"GTKType"
+    elif tag == GIRepository.TypeTag.GSLIST:
+        types.add('GTKType')
+        # param_type = type_info.get_param_type(0)
+        return f"GTKType"
+    elif tag == GIRepository.TypeTag.GHASH:
+        types.add('GTKType')
+        return "GTKType"
+    elif tag == GIRepository.TypeTag.ERROR:
+        types.add('GError')
+        return "GError"
+    elif tag == GIRepository.TypeTag.UNICHAR:
+        return "GTKType"
+    else:
+        return str(tag)
+
+def format_function_signature(func_info, class_name=None):
+    func_name = func_info.get_name()
+    symbol = func_info.get_symbol()
+    
+    return_type_info = func_info.get_return_type()
+    return_type = get_type_string(return_type_info)
+    
+    params = {}
+    n_args = func_info.get_n_args()
+
+    # Always add 'self' if this is a method
+    if hasattr(func_info, "get_flags"):
+        flags = func_info.get_flags()
+        if flags & GIRepository.FunctionInfoFlags.IS_METHOD:
+            container = func_info.get_container()
+            if container:
+                params["self"] = 'GTKInterface'
+
+    # Add other parameters
+    for i in range(n_args):
+        arg = func_info.get_arg(i)
+        arg_name = arg.get_name() or f"arg{i}"
+        arg_info = arg.get_type_info()
+        if arg_info is None:
+            arg_type = "void*"
+        else:
+            arg_type = get_type_string(arg_info)
+
+        direction = arg.get_direction()
+        if direction == GIRepository.Direction.OUT or direction == GIRepository.Direction.INOUT:
+            arg_type += "*"
+
+        params[arg_name] = arg_type
+
+    params_str = ", ".join(f"{t} {n}" for n, t in params.items()) if params else "void"
+
+    signature = f"{return_type} {symbol}({params_str})"
+    return signature, symbol, [return_type, symbol, params]
+
+
+SAFE_INFO_TYPES = (
+    GIRepository.FunctionInfo,
+    GIRepository.ObjectInfo,
+    GIRepository.InterfaceInfo,
+    GIRepository.StructInfo,
+    GIRepository.UnionInfo,
+    GIRepository.EnumInfo,
+    GIRepository.FlagsInfo,
+    GIRepository.CallbackInfo,
+)
+
+def extract_all_gtk_functions():
+    repo = GIRepository.Repository()
+    
+    try:
+        repo.require('cairo', '1.0', 0)        
+        repo.require('Gio', '2.0', 0)
+        repo.require('GObject', '2.0', 0)
+        repo.require('Gtk', '4.0', 0)
+
+        
+        all_functions = []
+        all_functions_dict = {}
+        stats = {
+            'top_level_functions': 0,
+            'object_methods': 0,
+            'object_constructors': 0,
+            'interface_methods': 0,
+            'struct_methods': 0,
+            'union_methods': 0,
+            'callbacks': 0,
+            'virtual_methods': 0,
+            'static_methods': 0
+        }
+        
+        print("=== Extracting ALL GTK 4.0 Functions with Signatures ===\n")
+        for ns in ["Gtk", "GObject", 'Gio']:
+            
+            n_infos = repo.get_n_infos(ns)
+            for i in range(n_infos):
+                try:
+                    info = repo.get_info(ns, i)
+                    if info is None:
+                        continue
+                    if not isinstance(info, SAFE_INFO_TYPES):
+                        continue
+                except:
+                    print('failed to get proper info, bad gi')
+                    continue
+
+                try:
+                    info_name = info.get_name()
+                except:
+                    print('failed to get name, bad gi')
+                    continue
+                # 1. Top-level functions
+                if isinstance(info, GIRepository.FunctionInfo):
+                    signature, symbol, raw_info = format_function_signature(info)
+                    # all_functions.append(f"{signature}\n")
+                    all_functions_dict[symbol] = {
+                        'rtype': raw_info[0],
+                        'params': raw_info[2]
+                    }
+                    stats['top_level_functions'] += 1
+                
+                # 2. Object/Class methods and constructors
+                elif isinstance(info, GIRepository.ObjectInfo):
+                    # Instance methods
+                    n_methods = info.get_n_methods()
+                    for j in range(n_methods):
+                        method = info.get_method(j)
+                        signature, symbol, raw_info = format_function_signature(method, info_name)
+                        
+                        flags = method.get_flags()
+                        if flags & GIRepository.FunctionInfoFlags.IS_METHOD:
+                            # all_functions.append(f"{signature}\n")
+                            all_functions_dict[symbol] = {
+                                    'rtype': raw_info[0],
+                                    'params': raw_info[2]
+                                }
+                            stats['object_methods'] += 1
+                        else:
+                            # all_functions.append(f"{signature}\n")
+                            all_functions_dict[symbol] = {
+                                    'rtype': raw_info[0],
+                                    'params': raw_info[2]
+                                }
+                            stats['static_methods'] += 1
+                    
+                    # Constructors
+                    try:
+                        n_constructors = info.get_n_constructors()
+                        for j in range(n_constructors):
+                            constructor = info.get_constructor(j)
+                            signature, symbol, raw_info = format_function_signature(constructor, f"{info_name}::new")
+                            # all_functions.append(f"{signature}\n")
+                            all_functions_dict[symbol] = {
+                                    'rtype': raw_info[0],
+                                    'params': raw_info[2]
+                                }
+                            stats['object_constructors'] += 1
+                    except:
+                        pass
+                    
+                    # Virtual methods (vfuncs)
+                    try:
+                        n_vfuncs = info.get_n_vfuncs()
+                        for j in range(n_vfuncs):
+                            vfunc = info.get_vfunc(j)
+                            try:
+                                signature, symbol, raw_info = format_function_signature(vfunc, f"{info_name}::vfunc")
+                                # all_functions.append(f"{signature}\n")
+                                all_functions_dict[symbol] = {
+                                    'rtype': raw_info[0],
+                                    'params': raw_info[2]
+                                }
+                                stats['virtual_methods'] += 1
+                            except:
+                                pass
+                    except:
+                        pass
+                
+                # 3. Interface methods
+                elif isinstance(info, GIRepository.InterfaceInfo):
+                    n_methods = info.get_n_methods()
+                    for j in range(n_methods):
+                        method = info.get_method(j)
+                        signature, symbol, raw_info = format_function_signature(method, info_name)
+                        # all_functions.append(f"{signature}\n")
+                        all_functions_dict[symbol] = {
+                                    'rtype': raw_info[0],
+                                    'params': raw_info[2]
+                                }
+                        stats['interface_methods'] += 1
+                    
+                    # Virtual methods in interfaces
+                    try:
+                        n_vfuncs = info.get_n_vfuncs()
+                        for j in range(n_vfuncs):
+                            vfunc = info.get_vfunc(j)
+                            try:
+                                signature, symbol, raw_info = format_function_signature(vfunc, f"{info_name}::vfunc")
+                                # all_functions.append(f"{signature}\n")
+                                all_functions_dict[symbol] = {
+                                    'rtype': raw_info[0],
+                                    'params': raw_info[2]
+                                }
+                                stats['virtual_methods'] += 1
+                            except:
+                                pass
+                    except:
+                        pass
+                
+                # 4. Struct methods
+                elif isinstance(info, GIRepository.StructInfo):
+                    n_methods = info.get_n_methods()
+                    for j in range(n_methods):
+                        method = info.get_method(j)
+                        signature, symbol, raw_info = format_function_signature(method, info_name)
+                        # all_functions.append(f"{signature}\n")
+                        all_functions_dict[symbol] = {
+                                    'rtype': raw_info[0],
+                                    'params': raw_info[2]
+                                }
+                        stats['struct_methods'] += 1
+                
+                # 5. Union methods
+                elif isinstance(info, GIRepository.UnionInfo):
+                    n_methods = info.get_n_methods()
+                    for j in range(n_methods):
+                        method = info.get_method(j)
+                        signature, symbol, raw_info = format_function_signature(method, info_name)
+                        # all_functions.append(f"{signature}\n")
+                        all_functions_dict[symbol] = {
+                                    'rtype': raw_info[0],
+                                    'params': raw_info[2]
+                                }
+                        stats['union_methods'] += 1
+                
+                # 6. Callback signatures
+                elif isinstance(info, GIRepository.CallbackInfo):
+                    try:
+                        signature, symbol, raw_info = format_function_signature(info, "Callback")
+                        # all_functions.append(f"{signature}\n  (Function pointer type)\n")
+                        all_functions_dict[symbol] = {
+                                    'rtype': raw_info[0],
+                                    'params': raw_info[2]
+                                }
+                        stats['callbacks'] += 1
+                    except:
+                        all_functions.append(f"{info_name} (function pointer type)\n")
+                        stats['callbacks'] += 1
+                
+                # Show progress
+                if (i + 1) % 50 == 0:
+                    print(f"Processed {i + 1}/{n_infos} items... Found {len(all_functions)} functions so far")
+            
+        # Write to file
+        with open('fn.json', 'w') as f:
+            # for func in all_functions:
+            all_functions_dict['unique_types'] = list(types)
+            f.write(json.dumps(all_functions_dict))
+            return all_functions_dict
+        
+    except Exception as e:
+        print(f"Error: {e}")
+        import traceback
+        traceback.print_exc()
+
+if __name__ == "__main__":
+    functions = extract_all_gtk_functions()
+    function_names = functions.keys()
+    mojosrc = []
+    for fns in function_names:
+        mojosrc.append(f'fn {fns}:\n\t')
+    
